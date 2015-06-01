1a42e0ebc47176478c4db29723fa9c51c1c1c048
Alex561 <alexchapp@yahoo.com>
2015-06-01 00:26:54 -0700

    made training match fight i  progress

--- MathFight/Assets/Scripts/batmananimation.cs
+++ MathFight/Assets/Scripts/batmananimation.cs
@@ -1,20 +1,35 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class batmananimation : MonoBehaviour {
-
+	public lessonremover lr;
+	public AudioClip punch;
+	private AudioSource source;
+	public Text answerC1;
 	Animator anim;
-	int punch=Animator.StringToHash("Punch");
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator> ();
+		source = GetComponent<AudioSource>();
+		anim= GetComponent<Animator> ();
+		lr = GameObject.Find("sandbag").GetComponent<lessonremover>();
+		answerC1.text = "Z: 1/2 \n" +
+			"X: sqrt(3)/2 \n" +
+				"C:0 \n" +
+				"V:1";
 	}
 
 	void Update ()
 	{
 		AnimatorStateInfo stateinfo = anim.GetCurrentAnimatorStateInfo (0);
-		if (Input.GetKeyDown (KeyCode.W)) {
+		if (Input.GetKeyDown (KeyCode.W)&& lr.ap1!=0)
+		{
+			
+			lr.ap1 -=1;
+			Debug.Log("w");
+			source.PlayOneShot(punch,5f);
 			anim.SetTrigger("Punch");
+			
 		}
 		else if (Input.GetKeyDown (KeyCode.S)) {
 			anim.SetTrigger("Block");
--- MathFight/Assets/Scripts/lessonremover.cs
+++ MathFight/Assets/Scripts/lessonremover.cs
@@ -5,14 +5,14 @@ public class lessonremover : MonoBehaviour {
 	//public string[] dialogue={"Math Lessons","This is a test"};
 	//int index=0;
 	//Rect dialoguerect=new Rect(435,100,500,50);
+	public int ap1;
 	public bool flip=false;
 	public string name;
 	public int input_count=0;
-	public int output_count=0;
-	Vector3 bleh=new Vector3(5.47f,-1.52f,-27.06f);
+	private AudioSource source;
 	// Use this for initialization
 	void Start () {
-
+		ap1 = 1;
 	}
 	//void OnGUI()
 	//{
@@ -31,8 +31,6 @@ public class lessonremover : MonoBehaviour {
 			
 			GetComponent<TextMesh>().text = "what is the angle for cos 60?";
 		}
-		//GetComponent<Collider>().attachedRigidbody.useGravity = true;
-		//GetComponent<Collider>().attachedRigidbody.isKinematic = false;
 		if(input_count==2)
 		{
 			GetComponent<TextMesh>().text = "what is the angle for sin 60?";
@@ -41,57 +39,23 @@ public class lessonremover : MonoBehaviour {
 
 	}
 	void Update () {
-		if (input_count == 3) {
+		if (input_count == 4) {
 			Application.LoadLevel("Fight");
 		}
-//		if (Input.GetKeyDown("space"))
-//			{
-//			 
-//			if(input_count==0)
-//			{
-//			GetComponent<TextMesh>().text = "what is the angle for tan 45?";
-//			}
-//
-//			if(input_count==1)
-//			{
-//				   
-//					GetComponent<TextMesh>().text = "what is the angle for cos 60?";
-//			}
-//			//GetComponent<Collider>().attachedRigidbody.useGravity = true;
-//			//GetComponent<Collider>().attachedRigidbody.isKinematic = false;
-//			if(input_count==2)
-//			{
-//				GetComponent<TextMesh>().text = "what is the angle for sin 60?";
-//			}
-//			input_count++;
-	//	}
 
-		if (Input.GetKeyDown (KeyCode.G)) {
+			if (Input.GetKeyDown(KeyCode.V)&& input_count ==1) { 
 
-			if (name == "1"&& output_count ==0) { 
-
-				Debug.Log("ok");
-				GetComponent<Collider> ().attachedRigidbody.position = bleh;
-				GetComponent<Collider> ().attachedRigidbody.isKinematic = true;
-				GetComponent<TextMesh> ().text = "correct";
+				ap1+=1;
 
 			}
-			else if((name=="1/2")&&(output_count==1))
+			else if((Input.GetKeyDown(KeyCode.Z))&&(input_count==2))
 			{
-				GetComponent<TextMesh> ().text = "correct";
+				ap1+=1;
 			}
-			else if(name=="s3/2"&&output_count==2)
+			else if(Input.GetKeyDown(KeyCode.X)&&input_count==3)
 			{
-				GetComponent<TextMesh> ().text = "correct";
+				ap1+=1;
 			}
-			else {
 
-				GetComponent<TextMesh> ().text ="Unit circle \n Angle	Sin	   Cos	 Tan=Sin/Cos \n 30°	1/2	  √3/2	 1√3 =  √33 \n 45°	√2/2	√2/2	1 \n 60°	√3/2	  1/2	√3";
-				input_count--;
-				output_count--;
-													
-			}
-			output_count++;
-		}
-}
+	}
 }
