4754599be342ef6927f229787e3830581189b320
warstark <smacha@uci.edu>
2015-06-03 02:36:51 -0700

    fiddling around with the ui ....
    confused as fuck

--- MathFight/Assets/Scripts/UIManagerScript.cs
+++ MathFight/Assets/Scripts/UIManagerScript.cs
@@ -6,11 +6,13 @@ public class UIManagerScript : MonoBehaviour {
 	public Canvas QuitMenu;
 	public Button startText;
 	public Button quitText;
+	public Button RulesText;
 
 	void Start(){
 		QuitMenu = QuitMenu.GetComponent<Canvas> ();
 		startText = startText.GetComponent<Button> ();
 		quitText = quitText.GetComponent<Button> ();
+		RulesText = RulesText.GetComponent<Button> ();
 		QuitMenu.enabled = false;
 
 	}
@@ -19,12 +21,14 @@ public class UIManagerScript : MonoBehaviour {
 		QuitMenu.enabled = true;
 		startText.enabled = false;
 		quitText.enabled = false;
+		RulesText.enabled = false;
 	}
 
 	public void NoPress(){
 		QuitMenu.enabled = false;
 		startText.enabled = true;
 		quitText.enabled = true;
+		RulesText.enabled = true;
 	}
 
 	public void StartGame(){
---
+++ MathFight/Assets/Scripts/ruledisplayscript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ruledisplayscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnGUI()
+	{
+		GUI.Label (new Rect (10, 10, 100, 20), "what are the controls lol?");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
